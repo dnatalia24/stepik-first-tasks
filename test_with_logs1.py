f3244afe63bc6a32d1f43f362691b58451798669
ndrozdovska <ndrozdovska@mains-MacBook-Pro.local>
2019-11-20 23:46:11 +0200

    updated version

--- test_with_logs1.py
+++ test_with_logs1.py
@@ -3,13 +3,13 @@ import unittest
 
 from selenium import webdriver
 import time
-
+#version1.0
 class TestReg(unittest.TestCase):
     def test_reg1(self):
-        link = "http://suninjuly.github.io/registration2.html"
+        link = "http://suninjuly.github.io/registration1.html"
         browser = webdriver.Chrome()
         browser.get(link)
-    # Ваш код, который заполняет обязательные поля
+    # fill in the mandatory fields
         input1 = browser.find_element_by_css_selector("input:required.first")
         input1.send_keys("Ivan")
         input2 = browser.find_element_by_css_selector("input:required.second")
@@ -17,16 +17,15 @@ class TestReg(unittest.TestCase):
         input3 = browser.find_element_by_css_selector("input:required.third")
         input3.send_keys("fortestonly@gmail.com")
 
-    # Отправляем заполненную форму
+    # sent the filled in form
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
+    # waiting for page loading
         time.sleep(1)
 
 
-        self.assertEqual(browser.find_element_by_tag_name("h1").text, "Поздравляем! Вы успешно зарегистировались!", 'Error Registration 1')
-
+        self.assertEqual(browser.find_element_by_tag_name("h1").text, "Congratulations! You have successfully registered!", 'Error Registration 1')
+        time.sleep(10)
 if __name__ == "__main__":
     unittest.main()
